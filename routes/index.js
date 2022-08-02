29b78fa07e1b3a2969e239c41ebc1dbc308443ff
pandupram <18102280@ittelkom-pwt.ac.id>
2022-08-02 18:11:03 +0700

    fix error

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import { refreshToken } from "../controllers/RefreshToken.js";
-import { getAllUsers, Login, Register } from "../controllers/UserController.js";
+import {
+  getAllUsers,
+  Login,
+  LogOut,
+  Register,
+} from "../controllers/UserController.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 
 const router = express.Router();
@@ -9,5 +14,6 @@ router.get("/api/users", verifyToken, getAllUsers);
 router.post("/api/users", Register);
 router.post("/login", Login);
 router.get("/token", refreshToken);
+router.delete("/logout", LogOut);
 
 export default router;
